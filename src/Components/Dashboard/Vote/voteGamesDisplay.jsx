353050f34f65a164e1724ae2560f4f3abdc39cf6
msilis <mikssilis@gmail.com>
2023-04-17 23:12:55 +0100

    Voting UI

--- src/Components/Dashboard/Vote/voteGamesDisplay.jsx
+++ src/Components/Dashboard/Vote/voteGamesDisplay.jsx
@@ -1,15 +1,16 @@
 import style from "./voteGamesDisplay.module.css";
 
-export default function VoteGamesDisplay({ voteProps }){
+export default function VoteGamesDisplay({ voteProps }) {
 
-    if (voteProps.loadingVote){
-        return <p>Loading...</p>
-    }else if (voteProps.votingGames.length === 0){
-        return <p>There are no games to vote on now</p>
-    }else {
-        return voteProps.votingGames.map((voteGame)=>{
-            return (
-                <div className={style.gameItem} key={voteGame._id} id={voteGame._id}>
+ 
+  if (voteProps.loadingVote) {
+    return <p>Loading...</p>;
+  } else if (voteProps.votingGames.length === 0) {
+    return <p>There are no games to vote on now</p>;
+  } else {
+    return voteProps.votingGames.map((voteGame, index) => {
+      return (
+        <div className={style.gameItem} key={index} id={voteGame._id}>
           <h4>{voteGame.gameName}</h4>
           <p>{voteGame.gameText}</p>
           <h5>Game focus:</h5>
@@ -19,16 +20,25 @@ export default function VoteGamesDisplay({ voteProps }){
             })}
           </div>
           <div className={style.voteCountDisplay}>
-            <p>Yes: {voteGame.yesVote}</p>
-            <p>No: {voteGame.noVote}</p>
+            <p>Yes: {voteProps.voteTotal.length === 0 ? "0" : voteProps.voteTotal[index].yesVote }</p>
+            <p>No: {voteProps.voteTotal.length === 0 ? "0" : voteProps.voteTotal[index].noVote}</p>
           </div>
-            <div className={style.voteButtonContainer}
+          <div className={style.voteButtonContainer}>
+            <button
+              className={style.voteYesButton}
+              onClick={voteProps.handleYesVote}
             >
-                <button className={style.voteYesButton} onClick={voteProps.handleYesVote}>Yes</button>
-                <button className={style.voteNoButton} onClick={voteProps.handleNoVote}>No</button>
-            </div>
+              Yes
+            </button>
+            <button
+              className={style.voteNoButton}
+              onClick={voteProps.handleNoVote}
+            >
+              No
+            </button>
+          </div>
         </div>
-            )
-        })
-    }
+      );
+    });
+  }
 }
