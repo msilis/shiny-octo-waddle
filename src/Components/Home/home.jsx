19f67ba8c73ae81971121e7ada66a6c9096d6d38
msilis <mikssilis@gmail.com>
2023-05-05 14:16:24 +0100

    Home section and small screen styling

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -6,11 +6,13 @@ export default function Home() {
     <div className={classnames(style.homeContainer, style.fadeContainer)}>
       <h2 className={style.homeHeading}>Welcome</h2>
       <p className={style.homeText}>
-        This isn't meant to replace your planning but it's more of a
-        memory jogger. The games and review that are suggested might be familiar
-        to you, but you might also get something you hadn't thought about.
-        That's the point. Come in and get some suggestions, try something new,
-        rediscover something you already knew.
+        This tool is designed to complement your planning efforts and serve as a
+        helpful reminder. While some of the suggested games and reviews may be
+        familiar to you, we hope that you will also discover something new that
+        you may not have considered before. Our goal is to provide you with
+        fresh ideas and inspiration, so feel free to come in and explore. You
+        may even rediscover something that you already know and love. Let's
+        embrace the joy of trying new things together!
       </p>
     </div>
   );
