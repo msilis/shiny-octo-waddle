b96d0c18cbaada54748b5c3d79acd1df8f015185
msilis <mikssilis@gmail.com>
2023-05-16 14:42:41 +0100

    MSW setup and login tests

---
+++ src/Testing/Mocks/handlers.js
@@ -0,0 +1,13 @@
+import { rest } from "msw";
+
+export const handlers = [
+  rest.post("http://localhost:8080/login", (req, res, ctx) => {
+    const userName = req.body.userName;
+    const password = req.body.password;
+    if (userName === "" || password === "") {
+      return res(ctx.status(401));
+    } else {
+      return res(ctx.status(200));
+    }
+  }),
+];
