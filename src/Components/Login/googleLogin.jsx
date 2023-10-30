a717ee9cf30ad79d8e0b42450e8fc48fddb1ca21
msilis <mikssilis@gmail.com>
2023-10-30 21:52:10 +0000

    remove console log

--- src/Components/Login/googleLogin.jsx
+++ src/Components/Login/googleLogin.jsx
@@ -3,11 +3,10 @@ import { STORAGE_OPTIONS } from "../../Utilities/Config/storage";
 import { ROUTE_PATHS } from "../../Utilities/Config/navigation";
 
 export const googleLoginSuccess = (credentialResponse, navigate) => {
-  console.log(credentialResponse);
   const tokenToDecode = credentialResponse.credential;
   const decodedToken = jwtDecode(tokenToDecode);
   sessionStorage.setItem(STORAGE_OPTIONS.googleLoginEmail, decodedToken.email);
   sessionStorage.setItem(STORAGE_OPTIONS.googleLoginName, decodedToken.name);
   sessionStorage.setItem(STORAGE_OPTIONS.googleLogin, true);
-  return navigate(ROUTE_PATHS.dashboard);
+  navigate(ROUTE_PATHS.dashboard);
 };
