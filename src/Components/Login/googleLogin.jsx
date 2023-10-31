65f920a3fba9ab483a68d67c9fec0acf54982712
msilis <mikssilis@gmail.com>
2023-10-31 10:01:38 +0000

    add working redirect when logging in with Google

--- src/Components/Login/googleLogin.jsx
+++ src/Components/Login/googleLogin.jsx
@@ -2,10 +2,18 @@ import { jwtDecode } from "jwt-decode";
 import { STORAGE_OPTIONS } from "../../Utilities/Config/storage";
 import { ROUTE_PATHS } from "../../Utilities/Config/navigation";
 
-export const googleLoginSuccess = (credentialResponse, navigate) => {
+export const googleLoginSuccess = (
+  credentialResponse,
+  navigate,
+  loginProps
+) => {
   const tokenToDecode = credentialResponse.credential;
   const decodedToken = jwtDecode(tokenToDecode);
   sessionStorage.setItem(STORAGE_OPTIONS.googleLoginEmail, decodedToken.email);
   sessionStorage.setItem(STORAGE_OPTIONS.googleLoginName, decodedToken.name);
   sessionStorage.setItem(STORAGE_OPTIONS.googleLogin, true);
+  loginProps.setLoggedIn(true);
+  loginProps.setLoading(false);
+
+  navigate(ROUTE_PATHS.dashboard);
 };
