1a9440c81369b12e6240b723fe3070345286f605
msilis <mikssilis@gmail.com>
2023-11-01 22:00:44 +0000

    remove duplicate key in savedCreatedGames

--- src/Components/SavedGames/savedCreatedGames.jsx
+++ src/Components/SavedGames/savedCreatedGames.jsx
@@ -38,7 +38,6 @@ export default function SavedCreatedGames({ userId }) {
     setMyGamesPagination,
     setCurrentGamesPage,
     currentGamesPage,
-    setMyGamesPagination,
     myGamesPagination,
     createdGamePageSize,
   };
