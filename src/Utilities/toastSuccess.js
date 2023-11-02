d6529fa1a53a8529558e078bc0ce29cae10dc35d
msilis <mikssilis@gmail.com>
2023-11-02 20:54:39 +0000

    remove alert and change toast time

--- src/Utilities/toastSuccess.js
+++ src/Utilities/toastSuccess.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 export function showSuccessToast(message) {
   toast.success(message, {
     position: "top-center",
-    autoClose: 5000,
+    autoClose: 3000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
