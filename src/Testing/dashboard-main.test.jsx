095851ca9bcc467336400aad7e9ef8f067afd8a2
msilis <mikssilis@gmail.com>
2023-11-06 09:20:59 +0000

    remove unneeded file and fix hardcoded url

--- src/Testing/dashboard-main.test.jsx
+++ src/Testing/dashboard-main.test.jsx
@@ -1,5 +1,6 @@
 import { it, vi, expect, describe } from "vitest";
 import { cleanup } from "@testing-library/react";
+import { API_URL } from "../Utilities/Config/api";
 
 describe("Dashboard-Main", () => {
   //Cleanup
@@ -12,16 +13,13 @@ describe("Dashboard-Main", () => {
       saveUser: "982j989d987d876",
     };
 
-    const gameResponse = await fetch(
-      "https://group-class-backend.onrender.com/saveGame",
-      {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-        },
-        body: JSON.stringify(gameData),
-      }
-    ).then((response) => {
+    const gameResponse = await fetch(API_URL.saveGame, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(gameData),
+    }).then((response) => {
       expect(response.status).toBe(201);
     });
   });
